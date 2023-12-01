4c7dc5e5fbc0d60271a9427d6bc6961c0ecdf279
Lucas Alves de Araujo <lucas.al.araujo@hotmail.com>
2023-12-01 12:40:29 -0300

    feat: document colors

--- packages/docs/.storybook/preview.ts
+++ packages/docs/.storybook/preview.ts
@@ -1,18 +1,14 @@
-import type { Preview } from "@storybook/react";
+import { themes } from '@storybook/theming'
 
-const preview: Preview = {
-  parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/i,
-      },
-    },
-    backgrounds: {
-      default: 'dark'
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
     },
   },
-};
-
-export default preview;
+  docs: {
+    theme: themes.dark,
+  }
+}
